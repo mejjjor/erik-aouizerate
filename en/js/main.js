51bd6e1863e8101c2fb417752e82fbb9eb8d80e4
mejjjor <erik.aouizerate@gmail.com>
2015-10-09 11:42:52 +0200

    update

--- en/js/main.js
+++ en/js/main.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
         $('.project-image').each(function(i) {
             var delay = 4000 + (Math.random() + (i % 4)) * 1000;
             $(this).slick({
+                lazyLoad: 'ondemand',
                 slidesToShow: 1,
                 slidesToScroll: 1,
                 dots: true,
