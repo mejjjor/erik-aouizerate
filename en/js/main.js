b54048e37c02100e986bb3ff0bc9b89619bdb48f
mejjjor <erik.aouizerate@gmail.com>
2015-10-09 11:23:07 +0200

    other shot ...

--- en/js/main.js
+++ en/js/main.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
         $('.project-image').each(function(i) {
             var delay = 4000 + (Math.random() + (i % 4)) * 1000;
             $(this).slick({
-                lazyLoad: 'ondemand',
                 slidesToShow: 1,
                 slidesToScroll: 1,
                 dots: true,
@@ -16,7 +15,7 @@ $(document).ready(function() {
                 autoplaySpeed: delay
             });
         });
-    }, 1500);
+    }, 500);
     $('nav a').on('click', function(event) {
         $('#menu-toggle').attr('checked', false);
     })
