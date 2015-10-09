3dca19c3f74a59567b187497335e14d2703ba7e0
mejjjor <erik.aouizerate@gmail.com>
2015-10-09 03:40:50 +0200

    add CV

--- static/js/main.js
+++ static/js/main.js
@@ -2,16 +2,19 @@ var $ = require('../../node_modules/jquery/dist/jquery.min.js');
 var slick = require('./slick.js');
 
 $(document).ready(function() {
-    $('.project-image').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        infinite: true,
-        arrows: false,
-        //autoplay: true,
-        autoplaySpeed: 3000
+    $('.project-image').each(function(i) {
+        var delay = 4000 + (Math.random()+(i%4))*1000;
+        $(this).slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            infinite: true,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: delay
+        });
     });
-    $('nav a').on('click',function(event){
+    $('nav a').on('click', function(event) {
         $('#menu-toggle').attr('checked', false);
     })
 
